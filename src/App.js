2c9f1c58b82da4840e9f80e78dcb066a293a2201
Nathan <nnoble2424@gmail.com>
2022-04-20 13:23:59 -0700

    final sub

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./App.css";
-
+import TitleBar from "./components/titleBar";
+import PictureBox from "./components/picture";
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+     <TitleBar/> 
+     <PictureBox/>
     </div>
   );
 }
---
+++ src/components/titleBar.js
@@ -0,0 +1,8 @@
+import React from "react";
+function TitleBar() {
+    return (
+        <div className="Title">
+            <h1>NASA Photo Of the Day</h1>
+        </div>);
+}
+export default TitleBar;
